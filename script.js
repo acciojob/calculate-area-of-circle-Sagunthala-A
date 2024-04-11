82d4168c1dd1fe0957e815c1d1273bf3b1ade06a
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-04-11 15:45:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,9 +5,10 @@
            // A = πr 2(area of circle)
 			 if(radius > 0){
 				 let pi = Math.PI;
-				 let rSq = Math.pow(r,2);
-				 let area = pi*rSq;
-				 console.log(area);
+				 let rSq = Math.pow(radius,2);
+				 let area =(pi*rSq).toFixed(2);
+				 
+				 alert("The area of the circle with radius "+ radius+" is "+area)
 			 }
 }
 calculateArea();
